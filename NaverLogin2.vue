19364c842ab645cff55e6f48d8da3688ae457c04
tejin3 <86044250+tejin3@users.noreply.github.com>
2021-12-01 20:12:55 +0900

    use api

---
+++ NaverLogin2.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div>안녕</div>
+    <div v-show="!user.email" id="naverIdLogin"></div>
+    <button v-show="user.email" @click="logout">로그아웃</button>
+  </div>
+</template>
+<script>
+// import axiios from "axios";
+import axios from "axios";
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      naverLogin: null,
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  setup() {},
+  created() {},
+  mounted() {
+    this.naverLogin = new window.naver.LoginWithNaverId({
+      clientId: "dOE3mjlgT_dOFfcbGK4W", //개발자센터에 등록된 클라이언트아이디
+      callbackUrl: "http://localhost:8080/naverlogin2", // 개발자센터에 등록한 콜백유알엘
+      isPopup: false,
+      loginButton: { color: "green", type: 3, height: 60 },
+      //   그린, 화이트 . 타입 1 , 2 , 3
+    });
+
+    this.naverLogin.init();
+    //init 이니셜라이즈 약자 초기화
+
+    // this.naverLogin.getLoginStatus(function(status))
+    // 함수 내에서 디스는 윈도우 객체 그래서 더이상 뷰 컴퍼넌트가 아니게 됨
+    // 근데 컴퍼넌트가 필요함. (스코피가 필요)
+
+    this.naverLogin.getLoginStatus((status) => {
+      if (status) {
+        console.log(status);
+        console.log(this.naverLogin.user);
+
+        this.$store.commit("user", this.naverLogin.user);
+      }
+    });
+  },
+  unmounted() {},
+  methods: {
+    logout() {
+      const accessToken = this.naverLogin.accessToken.accessToken;
+      const url =
+        "/oauth2.0/token?grant_type=delete&dOE3mjlgT_dOFfcbGK4W=jyvqXeaVOVmV&client_secret=e5FTvFLz2M&access_token=" +
+        accessToken +
+        "&service_provider=NAVER";
+
+      axios.get(url).then((res) => {
+        console.log(res);
+        this.$store.commit("user", {});
+        this.$router.push({ path: "/naverlogin2" });
+      });
+    },
+  },
+};
+</script>
---
+++ kakaologin2.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>{{ user.email ? "로그인 성공!" : "로그인 후 사용하세요." }}</h1>
+    <a v-show="!user.email" id="custom-login-btn" @click="kakaoLogin()">
+      <img
+        src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+        width="222"
+      />
+    </a>
+    <button v-show="user.email" @click="kakaoLogout">카카오 로그아웃</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    kakaoLogin() {
+      window.Kakao.Auth.login({
+        scope: "profile_nickname,account_email,gender",
+        success: this.getKakaoAccount,
+        fail: function (error) {
+          console.log(error);
+        },
+      });
+    },
+    getKakaoAccount() {
+      window.Kakao.API.request({
+        url: "/v2/user/me",
+        success: (res) => {
+          const kakaoAccount = res.kakao_account;
+          console.log(kakaoAccount);
+
+          this.$store.commit("user", kakaoAccount);
+          //   this.$store2.commit("user", kakaoAccount);
+          //   스토어에 접근할때 명령어는 무조건 $store.commit 파일이름 store2라고 store2로 쓰면 안됨.
+          // 메인에 폴더만 제대로 잡아주면 됨
+        },
+      });
+    },
+    kakaoLogout() {
+      if (!window.Kakao.Auth.getAccessToken()) {
+        console.log("Not logged in.");
+        return;
+      }
+      //로그인 되었는지 확인
+
+      window.Kakao.Auth.logout((res) => {
+        //펑션 빼고 ()안에res
+        console.log(window.Kakao.Auth.getAccessToken());
+        console.log(res);
+        this.$store.commit("user", {});
+      });
+    },
+  },
+};
+</script>
